013f40646f94be3aa9e4533a9a8be3c82ac3f002
Ajaya <kshettryajaya@gmail.com>
2022-10-25 15:24:20 +1100

    first commit

---
+++ array.js
@@ -0,0 +1,14 @@
+const strings = ["a", "s", "d", "f"];
+// 16 bytes of storage
+
+//push
+strings.push("g"); //0(1);
+
+//pop
+strings.pop();
+strings.pop(); //(1)
+
+strings.unshift('z')
+
+
+console.log(strings);
