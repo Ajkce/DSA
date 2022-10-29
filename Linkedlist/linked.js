bf35cb10f4b9c80d6af62db5a0c3c4258229c42e
Ajaya <kshettryajaya@gmail.com>
2022-10-29 15:49:13 +1100

    Linked List creation

--- Linkedlist/linked.js
+++ Linkedlist/linked.js
@@ -15,17 +15,44 @@
 // }
 
 class LinkedList {
-    constructor(value){
-        this.head = {
-            value: value,
-            next: null
-        }
-        this.tail = this.head;
-        this.length = 1;
- 
-    }
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this
+  }
 }
 
-const myLinkedList = new LinkedList(10)
+const myLinkedList = new LinkedList(10);
+console.log(myLinkedList.append(20));
+console.log(myLinkedList.append(50));
+console.log(myLinkedList.prepend(5));
 
-console.log(myLinkedList)
+console.log(myLinkedList);
