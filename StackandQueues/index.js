4c633425eb0d03433dd2ed719e7f1d7908214131
Ajaya <kshettryajaya@gmail.com>
2022-10-30 15:05:42 +1100

    Stack Creating with queues

--- StackandQueues/index.js
+++ StackandQueues/index.js
@@ -23,32 +23,38 @@ class Stack {
   }
   push(value) {
     const newNode = new Node(value);
-    if(this.length === 0){
-        this.top = newNode;
-        this.bottom = newNode
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
 
-    }else{
-        const holdingPointer = this.top
-        this.top = newNode
-      
-        this.top.next = holdingPointer
+      this.top.next = holdingPointer;
     }
     this.length++;
-    return this
+    return this;
   }
   pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.length === 1) {
+        this.bottom = null
+     
+    }
     const removing = this.top;
-    console.log(removing.next)
-    this.top = removing.next
+    this.top = removing.next;
     this.length--;
-    return this
-}
+    return this;
+  }
 }
 const mystack = new Stack();
 console.log(mystack);
-mystack.push('google');
-mystack.push('udemy');
-mystack.push('discord');
+mystack.push("google");
+mystack.push("udemy");
+mystack.push("discord");
+mystack.pop();
+mystack.pop();
+mystack.pop();
 console.log(mystack);
-console.log(mystack.peek())
-console.log(mystack.pop())
