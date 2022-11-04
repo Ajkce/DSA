b14e574b16582548dfb2b86b76474b96e5697cc4
Ajaya <kshettryajaya@gmail.com>
2022-11-04 15:00:34 +1100

    Sort with SelectionSort

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,12 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array){
+    let length = array.length;
+    for(let i = 0; i < length; i++){
+       
+    }
+
+}
+
+insertionSort(numbers);
+console.log(numbers)
