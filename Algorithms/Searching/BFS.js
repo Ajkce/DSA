3e340626460707478faa6e56fa4a2dbd965d582f
Ajaya <kshettryajaya@gmail.com>
2022-11-06 15:14:57 +1100

    BFS implementation

---
+++ Algorithms/Searching/BFS.js
@@ -0,0 +1,114 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  class BinarySearchTree {
+    constructor() {
+      this.root = null;
+    }
+    insert(value) {
+      const node = new Node(value);
+      if (this.root === null) {
+        this.root = node;
+      } else {
+        let currentNode = this.root;
+        while (true) {
+          if (value < currentNode.value) {
+            //left
+            if (!currentNode.left) {
+              currentNode.left = node;
+              return this;
+            }
+            currentNode = currentNode.left;
+          } else {
+            //right
+            if (!currentNode.right) {
+              currentNode.right = node;
+              return this;
+            }
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+  
+    lookup(value) {
+      if (!this.root) {
+        return false;
+      }
+      let currentNode = this.root;
+      while (currentNode) {
+        if (value < currentNode.value) {
+          currentNode = currentNode.left;
+        } else if (value > currentNode.value) {
+          currentNode = currentNode.right;
+        }
+        else if ((value = currentNode.value)) {
+          return currentNode;
+        }
+      }
+      return false;
+    }
+  
+    delete(value){
+      if (!this.root) {
+          return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while (currentNode) {
+          if (value < currentNode.value) {
+              parentNode = currentNode
+            currentNode = currentNode.left;
+          } else if (value > currentNode.value) {
+              parentNode = currentNode
+            currentNode = currentNode.right;
+          }
+          else if ((value === currentNode.value)) {
+             
+          }
+        }
+        return false;
+      
+    }
+
+    breadthFirstSearch(){
+        let currentNode = this.root;
+        let list = [];
+        let queue = [];
+        queue.push(currentNode);
+        while(queue.length > 0){
+            currentNode = queue.shift();
+            console.log(currentNode.value)
+            list.push(currentNode.value);
+            if(currentNode.left){
+                queue.push(currentNode.left)
+            }
+            if(currentNode.right){ 
+                queue.push(currentNode.right)
+            }
+            
+        }
+        return list;
+    }
+  }
+  
+  const tree = new BinarySearchTree();
+  tree.insert(9);
+  tree.insert(1);
+  tree.insert(19);
+  tree.insert(99);
+  tree.insert(2);
+  tree.insert(45);
+  tree.insert(15);
+  console.log(tree.lookup(99));
+  console.log(tree);
+  console.log(tree.breadthFirstSearch())
+  //       9
+  //  1        19
+  //2       15   99
+ //      45
---
+++ Algorithms/Sorting/sortQuestion.js
@@ -0,0 +1,23 @@
+//#1 - Sort 10 schools around your house by distance:
+//insertion sort since it is small data
+
+//#2 - eBay sorts listings by the current Bid amount:
+//radix or counting sort because it is all numbers
+
+//#3 - Sport scores on ESPN
+//Quick sort due to less space complexity
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+//Since data is very big and we wont be sorting on memory we use merge sort
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+//Insertion sort since it is almost sorted because previous reviews are almost sorted
+
+//#6 - Temperature Records for the past 50 years in Canada
+//Quick sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+//Quick sort
+
+//#8 - You want to teach sorting for the first time
+//Bubble sort, selection sort
